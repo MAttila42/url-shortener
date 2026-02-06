1f80b79831e6df9e463b436291f599dbadc29f15
MAttila42 <65223119+MAttila42@users.noreply.github.com>
2026-02-06 20:51:23 +0100

    feat: forever url

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import type { Env } from 'bun'
 import type { Context } from 'elysia'
 import * as crypto from 'node:crypto'
 import cors from '@elysiajs/cors'
-import { eq, lt } from 'drizzle-orm'
+import { and, eq, lt, ne } from 'drizzle-orm'
 import { Elysia, redirect, t } from 'elysia'
 import { db } from './db'
 import { Urls } from './db/schema'
@@ -35,22 +35,31 @@ const app = new Elysia({
       return status(404)
     }
 
-    (async () => await db.delete(Urls).where(lt(Urls.ttl, new Date())))()
+    (async () => await db
+      .delete(Urls)
+      .where(and(
+        lt(Urls.ttl, new Date()),
+        ne(Urls.ttl, new Date(0)),
+      )))()
 
     return redirect(result.url)
   })
   .post('/', async ({ body, status, request }) => {
+    const ttl = body.ttl
+      ? new Date(Date.now() + body.ttl)
+      : new Date (0)
+
     const [{ insertedId }] = await db.insert(Urls).values({
       id: await createId(),
       url: body.url,
-      ttl: new Date(Date.now() + body.ttl),
+      ttl,
     }).returning({ insertedId: Urls.id })
     const url = new URL(request.url)
     return status(201, `${url.origin}/${insertedId}`)
   }, {
     body: t.Object({
       url: t.String(),
-      ttl: t.Number(),
+      ttl: t.Optional(t.Number()),
     }),
   })
 
